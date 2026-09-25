246c328c2e090d2d87e1a02f0db7f597ef7a9d9f
Saran L <lsaran868@gmail.com>
2026-09-25 14:14:56 +0530

    fix(admin): add explicit PlaylistSong import in AdminController

--- backend/src/main/java/com/playx/controller/AdminController.java
+++ backend/src/main/java/com/playx/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.playx.controller;
 import com.playx.model.Album;
 import com.playx.model.Artist;
 import com.playx.model.Playlist;
+import com.playx.model.PlaylistSong;
 import com.playx.model.Song;
 import com.playx.model.User;
 import com.playx.repository.*;
